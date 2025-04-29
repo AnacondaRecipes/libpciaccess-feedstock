ce881f53224d32c9c007303108755cfd171f3598
skupr-anaconda <79282962+skupr-anaconda@users.noreply.github.com>
2025-04-29 18:00:17 +0300

    libpciaccess 0.18 (#1)
    Co-authored-by: skupriienko@anaconda.com <Serhii Kupriienko>

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,8 @@ set -ex
 meson setup builddir \
     ${MESON_ARGS} \
     --buildtype=release \
-    --prefix=$PREFIX
+    --prefix=$PREFIX \
+    --libdir=lib
 
 ninja -C builddir -j${CPU_COUNT}
 ninja -C builddir install
