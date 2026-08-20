c92d18f6aab08af3af756ee44d94b7ec10d5312d
avetr-work <avetroumov@anaconda.com>
2026-08-20 17:08:25 +0300

    PKG-16280: libpciaccess 0.19 (#3)
    * PKG-16280: libpciaccess 0.19
    
    * Fix linter errors
    
    * Fix build
    
    * Fix build.sh
    
    * Add zlib to the host section
    
    * Add exact pinning for zlib in host
    
    * Fix review findings

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,6 @@
 set -ex
-meson setup builddir \
-    ${MESON_ARGS} \
-    --buildtype=release \
-    --prefix=$PREFIX \
-    --libdir=lib
+
+meson setup builddir ${MESON_ARGS} -Dzlib=enabled
 
 ninja -C builddir -j${CPU_COUNT}
 ninja -C builddir install
