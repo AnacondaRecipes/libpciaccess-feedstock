eadc94853f322f6da82395a152dd70d17c7866f2
Andrii Vetroumov <avetroumov@anaconda.com>
2026-08-20 13:09:58 +0300

    Fix build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,6 @@
 set -ex
-meson setup builddir \
-    ${MESON_ARGS} \
-    --prefix=$PREFIX \
-    --libdir=lib
+
+meson setup builddir ${MESON_ARGS}
 
 ninja -C builddir -j${CPU_COUNT}
 ninja -C builddir install
